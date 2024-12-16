9f323aa836f2deb585a5ddd8a3317c350c04395d
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-12-16 13:11:55 +0200

    classes initial

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,7 +1,27 @@
 ﻿using System;
+using System.Numerics;
+
 namespace Lab
 {
+    interface IMyNumber<T> where T : IMyNumber<T>
+    {
+        T Add(T b);
+        T Subtract(T b);
+        T Multiply(T b);
+        T Divide(T b);
+    }
 
+    class MyFrac
+    {
+        private BigInteger num;
+        private BigInteger den;
+    }
+
+    class MyComplex
+    {
+        private double real;
+        private double imaginary;
+    }
     class Program
     {
         static void Main(string[] args)
