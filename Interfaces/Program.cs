ee0527d70cf693f96c5e1bb1c334888885e03099
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-12-16 13:37:21 +0200

    constructors and ToString for MyFrac

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -13,8 +13,41 @@ namespace Lab
 
     class MyFrac
     {
-        private BigInteger num;
+        private BigInteger nom;
         private BigInteger den;
+
+        private BigInteger Evklid(BigInteger f, BigInteger s)
+        {
+            while (s != 0)
+            {
+                BigInteger temp = s;
+                s = f % s;
+                f = temp;
+            }
+            return f;
+        }
+        public MyFrac(BigInteger nom, BigInteger den)
+        {
+            if (den == 0) throw new Exception("denom is zero");
+            BigInteger nsd = Evklid(nom, den);
+            this.nom = nom/nsd;
+            this.den = den/nsd;
+        }
+
+        public MyFrac(string inp) : this(long.Parse(inp.Split('/')[0]), BigInteger.Parse(inp.Split('/')[1]))
+        {
+        }
+
+        public override string ToString()
+        {
+            if (nom == 0) return "0";
+            else if (nom < 0 && den < 0)
+                return $"{BigInteger.Abs(nom)}/{BigInteger.Abs(den)}";
+            else if (nom < 0 || den < 0)
+                return $"-({BigInteger.Abs(nom)}/{BigInteger.Abs(den)})";
+            else
+                return $"{nom}/{den}";
+        }
     }
 
     class MyComplex
